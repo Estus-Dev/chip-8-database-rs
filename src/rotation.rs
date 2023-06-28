eefb47922974d12f7c93ea102093331dde9285f4
Estus <git@estus.dev>
2023-06-28 17:36:44 -0600

    docs: pull in schema description of rotation

--- src/rotation.rs
+++ src/rotation.rs
@@ -1,5 +1,6 @@
 use serde_repr::{Deserialize_repr, Serialize_repr};
 
+/// The screen orientation, in degrees rotated clockwise from the normal position.
 #[derive(Clone, Debug, Default, Deserialize_repr, Eq, Hash, PartialEq, Serialize_repr)]
 #[repr(usize)]
 pub enum ScreenRotation {
