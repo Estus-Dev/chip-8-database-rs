0be32a927bf5d59409a96461f591041e4320932f
Estus <git@estus.dev>
2023-06-28 17:35:25 -0600

    docs: pull my fork's schema descriptions for quirk

--- src/quirk.rs
+++ src/quirk.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 
+/// An ID for each quirk, by which to reference it in a [Program] or [Platform].
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub enum Quirk {
@@ -13,14 +14,26 @@ pub enum Quirk {
     Logic,
 }
 
+/// A detailed breakdown of the meaning of a [Quirk].
 #[cfg(feature = "quirks")]
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct QuirkDetails {
+    /// An ID for each quirk, by which to reference it in a [Program] or [Platform].
     pub id: Quirk,
+
+    /// The name of this quirk.
     pub name: String,
+
+    /// A description of the quirk and why it exists.
     pub description: Option<String>,
+
+    /// Whether or not this quirk should be enabled by default.
     pub default: bool,
+
+    /// A description of CHIP-8 behavior when this quirk is enabled.
     pub if_true: String,
+
+    /// A description of CHIP-8 behavior when this quirk is not enabled.
     pub if_false: String,
 }
