7d7d81dee4d40a28dbcdc5a806bd69af875267fe
Estus <git@estus.dev>
2023-06-28 18:39:06 -0600

    docs: describe rotations

--- src/rotation.rs
+++ src/rotation.rs
@@ -6,9 +6,16 @@ use serde_repr::{Deserialize_repr, Serialize_repr};
 #[derive(Clone, Debug, Default, Deserialize_repr, Eq, Hash, PartialEq, Serialize_repr)]
 #[repr(usize)]
 pub enum ScreenRotation {
+    /// A landscape view - the default orientation.
     #[default]
     Landscape = 0,
+
+    /// A portrait view - rotated 90 degrees clockwise from the default.
     Portrait = 90,
+
+    /// A landscape view - rotated 180 degrees clockwise from the default.
     LandscapeFlipped = 180,
+
+    /// A portrait view - rotated 270 degrees clockwise from the default.
     PortraitFlipped = 270,
 }
