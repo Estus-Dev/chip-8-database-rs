44d2a1b898fc6f9104c4510d0c3a87e49d4a11b0
Estus <git@estus.dev>
2023-06-28 18:10:38 -0600

    docs: describe each font family

--- src/font.rs
+++ src/font.rs
@@ -7,12 +7,26 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Default, Deserialize, Eq, Hash, PartialEq, Serialize)]
 #[serde(rename_all = "lowercase")]
 pub enum FontStyle {
+    /// The font used on the original [COSMAC VIP](https://en.wikipedia.org/wiki/COSMAC_VIP)
+    /// hobbyist computer.
     #[default]
     VIP,
+
+    /// The font used by the [Octo IDE](http://johnearnest.github.io/Octo/)'s built-in emulator.
     Octo,
+
+    /// The font used by the SCHIP interpreter for the HP-48 calculator.
     SCHIP,
+
+    /// The font used on the DREAM 6800 hobbyist computer.
     Dream6800,
+
+    /// The font used on the ETI 660 hobbyist computer.
     ETI660,
+
+    /// The font used by the Fish N Chips emulator.
     Fish,
+
+    /// Unknown, possibly a font used in ROMs by AKouZ1?
     Akouz1,
 }
